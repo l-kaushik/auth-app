7e623bdeda039a1ab7311ed566044fb0953e4241
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2026-04-26 16:16:57 +0530

    initial commit

---
+++ auth-app-backend/src/main/java/in/lokeshkaushik/authapp/AuthAppBackendApplication.java
@@ -0,0 +1,13 @@
+package in.lokeshkaushik.authapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAppBackendApplication.class, args);
+	}
+
+}
